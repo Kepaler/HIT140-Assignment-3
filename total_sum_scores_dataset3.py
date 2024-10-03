7e8ef03d32274d8f73f9cc1c001a8607078fc76a
Blake <105617499+Kepaler@users.noreply.github.com>
2024-10-03 13:56:37 +0930

    Add files via upload

---
+++ total_sum_scores_dataset3.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.read_csv('dataset3.csv')
+
+# Skip the first column of a row and obtain rest of data in row
+answer_columns = df.columns[1:] 
+
+# List to store sum values for each participant
+participant_sums = []
+
+# Calculate sum for each answer by participant (row) and append to the list
+for answer, row in df.iterrows():
+    
+    # Skip the first column (participant ID) and sum the rest of the answers
+    participant_sum = row[answer_columns].sum()  
+    participant_sums.append(participant_sum)
+
+# Add the total sum for all answers to the participant_sums variable
+total_sum_all_participants = sum(participant_sums)
+participant_sums.append(total_sum_all_participants)
+
+# Print the list of sum values
+print(participant_sums)
